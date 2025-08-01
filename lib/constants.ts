37d40a9a2a9e5eb9f0f74ae075522df33466844c
Md. Shohel Rana <147373728+ShohelEcho@users.noreply.github.com>
2025-08-01 20:05:59 +0600

    create product details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Description '
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || 'https://ronodip.com'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
